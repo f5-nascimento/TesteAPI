f21416c3041fbcb7cbde1f871d8b9e88f52c4d6e
Felippe Nascimento <nascimento.conceito@gmail.com>
2025-07-06 11:15:29 -0300

    correção no cors

--- BackEnd/api/login.js
+++ BackEnd/api/login.js
@@ -1,6 +1,15 @@
 const { Client } = require('pg');
 
 module.exports = async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://dedalobuilders.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
